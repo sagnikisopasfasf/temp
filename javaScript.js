8cb1eaeca5948e6986a411d505451a19030f897f
sagnikisopasfasf <palsagnik1994@gmail.com>
2025-04-04 23:28:06 +0530

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,88 @@
+// // console.log("sagnik")
+// // fullname="Sagnik pal"
+// // console.log(fullname)
+
+// // const product={
+// //     title:"Ball pen",
+// //     rating:4,
+// //     offer:5,
+// //     price:250,
+// // };
+// // console.log(product);
+
+// // const profile={
+// //     isfollow:true,
+// //     posts:0,
+// //     followers:114,
+// //     following:96,
+// //     username:"@sagnik_is_op" 
+// // };
+// // console.log(profile);
+
+// // let no=prompt("Enter a number")
+// // if(no%5===0)
+// //     console.log("true")
+// // else
+// //     console.log("false")
+
+// // let s=prompt("Enter the number")
+// // if(s>=90 && s<=100)
+// //     console.log("A")
+// // else if(s>=70 && s<=89)
+// //     console.log("B")
+// // else if(s>=60 && s<=69)
+// //     console.log("C")
+// // else if(s>=50 && s<=59)
+// //     console.log("D")
+// // else 
+// //     console.log("F")
+// // let special=`this is a special string ${1+2+3}`
+// // console.log(special)
+
+// // let n=prompt("Enter the name")
+// // w=" "+n.trim()+n.length
+// // console.log("username =",w.replace(" ","@"))
+
+// // let marks=[85,97,34]
+// // let sum=0
+// // for(i=0;i<marks.length;i++)
+// // sum+=marks[i];
+// // console.log("averagee=",sum/=3)
+
+// // let price=[85,97,34]
+// // for(let val=0;val<price.length;val++)
+// // {
+// //     v=(price[val]*10)/100.0
+// //     price[val]-=v
+// //     console.log(price[val])
+// // }
+
+// // function cal()
+// // {
+// //     console.log("THIS IS MY FUCNTION")
+// // }
+// // cal()
+
+// // const arow=(a,b)=>{
+// //     console.log(a+b); //a+b;
+// // };
+
+// // const count_vowels=(str)=>{
+// //     str=str.toUpperCase();
+// //     c=0;
+// //     for(let i=0;i<str.length;i++){
+// //         ch=str.charAt(i);
+// //         if(ch==='A'||ch==='E'||ch==='I'||ch==='O'||ch==='U'){
+// //             c++;
+// // //         }
+            
+// //     }
+// //     console.log("count=",+c);
+// // }
+// // let mark=[97,64,55,35,98,24];
+// // let m= mark.filter((a)=>{
+// //     return a>90;
+// // });
+// // console.log(m);
+// let div=document.querySelector("div");
+// console.dir(div);
